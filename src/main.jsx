4ac8d376b99f36ff822a04fd621dfd50b1673928
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-12-15 15:15:56 +0530

    added redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import store from "./store";
+import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </>
 );
