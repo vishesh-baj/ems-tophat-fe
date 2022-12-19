aab4151e145d9134342f0da5c474bc75c83810ff
manishtophat <manish.tophat@gmail.com>
2022-12-19 16:44:36 +0530

    added changes

---
+++ src/pages/CandidatePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CandidatePage = () => {
+  return (
+    <div>
+      CandidatePage
+      <button className="bg-red-900" onClick={handlelogout}>
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default CandidatePage;
